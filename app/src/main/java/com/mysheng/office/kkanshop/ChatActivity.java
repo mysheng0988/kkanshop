4acc3f7c9b0ce95371ab7598f04fe39ed259b675
mysheng0988 <mysheng185@163.com>
2018-06-06 23:33:30 +0800

    语音和文字发送

--- app/src/main/java/com/mysheng/office/kkanshop/ChatActivity.java
+++ app/src/main/java/com/mysheng/office/kkanshop/ChatActivity.java
@@ -7,12 +7,15 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.mysheng.office.kkanshop.adapter.ChatAdapter;
 import com.mysheng.office.kkanshop.entity.ChatModel;
-import com.mysheng.office.kkanshop.entity.Recorder;
 import com.mysheng.office.kkanshop.view.AudioRecorderButton;
 import com.mysheng.office.kkanshop.view.MediaManager;
 
@@ -23,11 +26,15 @@ import java.util.List;
  * Created by myaheng on 2017/12/15.
  */
 
-public class ChatActivity extends Activity {
+public class ChatActivity extends Activity implements View.OnClickListener{
     private RecyclerView recyclerView;
+    private static boolean isKeyboard=false;//默认显示切换语音
     private TextView titleText;
     private ImageButton backButton;
+    private ImageView keyboard;
+    private ImageView sendOut;
     private ChatAdapter chatAdapter;
+    private EditText audioText;
     private List<ChatModel> mDatas = new ArrayList<>();
     private AudioRecorderButton mAudioRecorderButton;
     private View animView;
@@ -36,18 +43,24 @@ public class ChatActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.audio_chat);
-        titleText=findViewById(R.id.common_title);
-        titleText.setText("聊天页面");
-        recyclerView=findViewById(R.id.recyclerView);
-        backButton=findViewById(R.id.btn_back);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        initView();
+        initEvent();
+
         LinearLayoutManager layoutManager=new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
         recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                    if(bottom<oldBottom){
+                        recyclerView.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                recyclerView.scrollToPosition(chatAdapter.getItemCount()-1);
+                            }
+                        },100);
+                    }
+            }
+        });
         chatAdapter=new ChatAdapter(this);
         chatAdapter.setItemClickListener(new ChatAdapter.OnItemClickListener() {
             @Override
@@ -78,13 +91,11 @@ public class ChatActivity extends Activity {
                         }
                     });
                 }
-
-
             }
 
         });
         recyclerView.setAdapter(chatAdapter);
-        mAudioRecorderButton =  findViewById(R.id.id_recorder_button);
+
         mAudioRecorderButton.setAudioFinishRecorderListener(new AudioRecorderButton.AudioFinishRecorderListener() {
             @Override
             public void onFinish(float seconds, String filePath) {
@@ -103,6 +114,30 @@ public class ChatActivity extends Activity {
         initData();
 
     }
+    private void initView(){
+        titleText=findViewById(R.id.common_title);
+        keyboard=findViewById(R.id.keyboard);
+        titleText.setText("聊天页面");
+        recyclerView=findViewById(R.id.recyclerView);
+        backButton=findViewById(R.id.btn_back);
+        audioText=findViewById(R.id.audio_text);
+        sendOut=findViewById(R.id.send_out);
+        mAudioRecorderButton =  findViewById(R.id.id_recorder_button);
+    }
+    private void initEvent(){
+        keyboard.setOnClickListener(this);
+        sendOut.setOnClickListener(this);
+        audioText.setOnFocusChangeListener(new android.view.View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(ChatActivity.this.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(), 0);
+                }
+            }
+        });
+        backButton.setOnClickListener(this);
+    }
     private void initData(){
         String str="测试123";
         String connect="";
@@ -119,14 +154,56 @@ public class ChatActivity extends Activity {
             connect+=str+i;
             ChatModel chatModel=new ChatModel();
             chatModel.type=type;
-           // chatModel.content=connect;
             mDatas.add(chatModel);
         }
         chatAdapter.addList(mDatas);
         chatAdapter.notifyDataSetChanged();
-        mDatas.clear();
+        recyclerView.scrollToPosition(chatAdapter.getItemCount()-1);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_back:
+                finish();
+                break;
+            case R.id.keyboard:
+                switchToTextAndAudio();
+                break;
+            case R.id.send_out:
+                sendOutText();
+                break;
+
+        }
+    }
+    private void sendOutText(){
+        String strText=audioText.getText().toString().trim();
+        if("".equals(strText)){
+            Toast.makeText(ChatActivity.this,"发送内容不能为空",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        ChatModel chatModel=new ChatModel();
+        chatModel.type=2;
+        chatModel.content=audioText.getText().toString().trim();
+        chatAdapter.addModel(chatModel);
+        chatAdapter.notifyDataSetChanged();
+        recyclerView.scrollToPosition(chatAdapter.getItemCount()-1);
+        audioText.setText("");
     }
 
+    private void switchToTextAndAudio(){
+        if(isKeyboard){
+            isKeyboard=false;
+            keyboard.setImageResource(R.drawable.audio);
+            audioText.setVisibility(View.VISIBLE);
+            mAudioRecorderButton.setVisibility(View.GONE);
+        }else{
+            isKeyboard=true;
+            audioText.clearFocus();
+            keyboard.setImageResource(R.drawable.keyboard);
+            audioText.setVisibility(View.GONE);
+            mAudioRecorderButton.setVisibility(View.VISIBLE);
+        }
+    }
     @Override
     protected void onPause() {
         super.onPause();
@@ -144,7 +221,5 @@ public class ChatActivity extends Activity {
         super.onDestroy();
         MediaManager.release();
     }
-
-
 
 }
