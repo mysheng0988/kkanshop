c4e51845fb2ac414b03d8e8076be7cc8e87dfc50
mysheng0988 <mysheng185@163.com>
2019-01-02 17:53:53 +0800

    增加视频播放

--- app/src/main/java/com/mysheng/office/kkanshop/DescribeActivity.java
+++ app/src/main/java/com/mysheng/office/kkanshop/DescribeActivity.java
@@ -4,14 +4,22 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.mysheng.office.kkanshop.adapter.UserAdapter;
 import com.mysheng.office.kkanshop.adapter.ViewLineDivider;
+import com.mysheng.office.kkanshop.customVideoPlayer.NiceVideoPlayer;
+import com.mysheng.office.kkanshop.customVideoPlayer.NiceVideoPlayerManager;
+import com.mysheng.office.kkanshop.customVideoPlayer.TxVideoPlayerController;
 import com.mysheng.office.kkanshop.entity.UserModel;
+import com.mysheng.office.kkanshop.permissions.RxPermissions;
 import com.mysheng.office.kkanshop.util.UtilToast;
 import com.mysheng.office.kkanshop.utils.PhoneUtils;
 import com.mysheng.office.kkanshop.utils.Utils;
@@ -28,7 +36,7 @@ import io.reactivex.functions.Consumer;
  * Created by myaheng on 2018/9/6.
  */
 
-public class DescribeActivity extends BaseActivity{
+public class DescribeActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageView comeBack;
 
     private MarqueeTextView marqueeTextView;
@@ -37,16 +45,25 @@ public class DescribeActivity extends BaseActivity{
     private List<UserModel> users=new ArrayList<>();
 
     private UserAdapter adapter;
+    public RxPermissions rxPermissions;
+    private NiceVideoPlayer mNiceVideoPlayer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //方式一：这句代码必须写在setContentView()方法的后面
+        getSupportActionBar().hide();
+
+        //方式二：这句代码必须写在setContentView()方法的前面
+        //supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        rxPermissions = new RxPermissions(this);
         setContentView(R.layout.describe_layout);
         Utils.init(DescribeActivity.this);
         initView();
         initEvent();
 
-
+        init();
     }
 
 
@@ -93,7 +110,22 @@ public class DescribeActivity extends BaseActivity{
        // addMenu.setOnClickListener(this);
 
     }
-
+    private void init() {
+        mNiceVideoPlayer = (NiceVideoPlayer) findViewById(R.id.niceVideoPlayer);
+        mNiceVideoPlayer.setPlayerType(NiceVideoPlayer.TYPE_IJK); // IjkPlayer or MediaPlayer
+        String videoUrl = "http://play.g3proxy.lecloud.com/vod/v2/MjUxLzE2LzgvbGV0di11dHMvMTQvdmVyXzAwXzIyLTExMDc2NDEzODctYXZjLTE5OTgxOS1hYWMtNDgwMDAtNTI2MTEwLTE3MDg3NjEzLWY1OGY2YzM1NjkwZTA2ZGFmYjg2MTVlYzc5MjEyZjU4LTE0OTg1NTc2ODY4MjMubXA0?b=259&mmsid=65565355&tm=1499247143&key=f0eadb4f30c404d49ff8ebad673d3742&platid=3&splatid=345&playid=0&tss=no&vtype=21&cvid=2026135183914&payff=0&pip=08cc52f8b09acd3eff8bf31688ddeced&format=0&sign=mb&dname=mobile&expect=1&tag=mobile&xformat=super";
+//        videoUrl = Environment.getExternalStorageDirectory().getPath().concat("/办公室小野.mp4");
+        mNiceVideoPlayer.setUp(videoUrl, null);
+        TxVideoPlayerController controller = new TxVideoPlayerController(this);
+        controller.setTitle("办公室小野开番外了，居然在办公室开澡堂！老板还点赞？");
+        controller.setLenght(98000);
+        Glide.with(this)
+                .load("http://tanzi27niu.cdsb.mobi/wps/wp-content/uploads/2017/05/2017-05-17_17-30-43.jpg")
+                .placeholder(R.drawable.img_default)
+                .crossFade()
+                .into(controller.imageView());
+        mNiceVideoPlayer.setController(controller);
+    }
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -104,19 +136,42 @@ public class DescribeActivity extends BaseActivity{
                 break;
         }
     }
+    public void enterTinyWindow(View view) {
+        if (mNiceVideoPlayer.isIdle()) {
+            Toast.makeText(this, "要点击播放后才能进入小窗口", Toast.LENGTH_SHORT).show();
+        } else {
+            mNiceVideoPlayer.enterTinyWindow();
+        }
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        NiceVideoPlayerManager.instance().releaseNiceVideoPlayer();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (NiceVideoPlayerManager.instance().onBackPressd()) return;
+        super.onBackPressed();
+    }
     @Override
     protected void onResume() {
         super.onResume();
-        if(marqueeTextView!=null)
-      marqueeTextView.startScroll();
+        if(marqueeTextView!=null){
+            getSupportActionBar().hide();
+            marqueeTextView.startScroll();
+        }
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if(marqueeTextView!=null)
-        marqueeTextView.stopScroll();
+        if(marqueeTextView!=null){
+            marqueeTextView.stopScroll();
+        }
+
     }
 
     @Override
